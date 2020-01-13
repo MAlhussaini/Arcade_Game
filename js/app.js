2c6c2328789c00e8d5e88c88dd91577eb6c16350
MAlhussaini <MAlhussaini56@gmail.com>
2020-01-13 21:11:06 +0300

    The player is up and running.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,14 +10,14 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -26,16 +26,61 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 class Player {
+    constructor() {
+        // x = -55, and y = -86 is the top left corner
+        // the center of the top left corner x = 0, and y = -43
+        // the multiplayer to go to the bottom corner x = 9, and y = 11
+        // the center of the player is 
+        this.x = -50.5 + 50.5 * 5;
+        this.y = -83 + 41.5 * 11;
+        this.sprite = "images/char-boy.png";
+    }
+
     update() {
 
     }
 
     render() {
 
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
-    handleInput() {
+    handleInput(direction) {
+        switch (direction) {
+            case "left":
+                if (this.x === 0) {
+                    this.x = 404;
+                    break;
+                }
+                this.x -= 101;
+                break;
+            case "right":
+                if (this.x === 404) {
+                    this.x = 0;
+                    break;
+                }
+                this.x += 101;
+                break;
+            case "up":
+                if (this.y === 41.5) {
+                    this.x = 202;
+                    this.y = 373.5;
+                    console.log("You Won!")
+                    break;
+                }
+                this.y -= 83;
+                break;
+            case "down":
+                if (this.y === 373.5) {
+                    break;
+                }
+                this.y += 83;
+                break;
+        }
+        console.log(this.x)
+        console.log(this.y)
 
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
@@ -48,7 +93,7 @@ let player = new Player;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -58,3 +103,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// TODO: if sqrt(x^2 + y^2) < radius then: collusion.
